766b250cd9a64eb6d3b3070335939c22f812ec7f
CrystaL <80718023+CodingWithCrystaL@users.noreply.github.com>
2026-04-18 20:46:16 +0530

    Delete website directory

--- website/js/config.js
+++
@@ -1,7 +0,0 @@
-const API_BASE = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" 
-    ? "http://localhost:8000" 
-    : "/api";
-const DISCORD_CLIENT_ID = "1491956010938535978";
-const REDIRECT_URI = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
-    ? "http://localhost:5500/auth/callback"
-    : `https://${window.location.hostname}/auth/callback`;
